e5ac35491630b549e4426b8f9728beda28a9ca0a
janeesh <janeesh.tin@gmail.com>
2022-09-23 19:26:41 +0530

    minimal changes

---
+++ demowebsite/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <About />
+            <Footer />
+        </>
+    )
+}
+
+export default App
