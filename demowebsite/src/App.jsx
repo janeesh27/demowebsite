e913aef5b2c3e384c62de3b4c126cdb60ea96c0a
janeesh <janeesh.tin@gmail.com>
2022-09-27 01:04:38 +0530

    navbar added

--- demowebsite/src/App.jsx
+++ demowebsite/src/App.jsx
@@ -6,13 +6,11 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
     return (
 
-        <>
+        
 
-            <Routes>
-                <Route path='/' element={<Navbar />} />
-            </Routes>
+           <Navbar />
 
-        </>
+     
 
 
     )
