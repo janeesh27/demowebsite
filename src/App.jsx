4b8f0d6b29c2a38d5736ad97386f2c0d100ac80e
janeesh <janeesh.tin@gmail.com>
2022-09-29 03:00:18 +0530

    added media and body section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Body from './components/Body/Body'
 // import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 const App = () => {
     return (
 
-        
+        <>
+            <Navbar />
+            <Body />
 
-           <Navbar />
+        </>
 
-     
+
+
 
 
     )
