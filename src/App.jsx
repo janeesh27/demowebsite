5f98b3eef769f908683b54400d22f7a0a1a8cb6d
janeesh <janeesh.tin@gmail.com>
2022-09-30 00:01:52 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Body from './components/Body/Body'
+import Footer from './components/Footer/Footer'
 // import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
@@ -10,6 +11,7 @@ const App = () => {
         <>
             <Navbar />
             <Body />
+            <Footer />
 
         </>
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="main-footer">
+      <div className="copyright">
+        <h3>✦The Indegenous</h3>
+        <p>
+          The Indegenous is a non-profit association under the Loi 1901 in
+          France, recognized as being of public utility and registered in the
+          "Journal Officiel" under the number W343029367. The Indegenous is
+          approved.
+        </p>
+      </div>
+      <div className="contacts">
+        <h3>Contact us</h3>
+        <p>66 Ave. des Champs-Élysées 75008 Paris France</p>
+        <p>iwantin@theindegenous.org</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
