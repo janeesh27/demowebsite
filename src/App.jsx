80ed7e343a55581f60294ecf43e515f2e39159f3
janeesh <janeesh.tin@gmail.com>
2022-09-30 05:30:43 +0530

    form and buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Body from './components/Body/Body'
 import Footer from './components/Footer/Footer'
+import Form from './components/Form/Form'
 // import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
@@ -11,6 +12,7 @@ const App = () => {
         <>
             <Navbar />
             <Body />
+            <Form />
             <Footer />
 
         </>
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Form.css";
+
+const Form = () => {
+  return (
+    <>
+      <div className="userform">
+        <form action="">
+          <div>
+            <label htmlFor="username">Full Name</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Your Name.."
+            />
+          </div>
+          <div>
+            <label htmlFor="Email">Email Address</label>
+            <input
+              type="text"
+              name="Email"
+              id="Email"
+              placeholder="theindegenous@gmail.com"
+            />
+          </div>
+          <div>
+            <label htmlFor="Country">Country</label>
+            <input
+              type="text"
+              name="country"
+              id="country"
+              placeholder="Ex:India,USA,France..."
+            />
+          </div>
+          <div>
+            <label htmlFor="Discription">Reason You Want to Join</label>
+            <input
+              type="text"
+              name="reason"
+              id="reason"
+              placeholder="150-200 words... "
+            />
+          </div>
+          <div>
+            <input type="submit" value="Register" />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
